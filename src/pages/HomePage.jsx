8507eb0b113b5f7f62689cae8a8498ad3decff31
Maxime Brunet <maximb84@gmail.com>
2024-09-09 15:37:03 +0200

    style : ajout galerie logement page accueil

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,18 @@ import banner from "../assets/banner.png";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
+import Gallery from '../components/Gallery/Gallery';
 const Home = () => {
     return (
         <div className="homepage">
             <Header />
-            <Banner img={banner} text="Chez vous, partout et ailleurs" />
+            <main>
+                <Banner img={banner} text="Chez vous, partout et ailleurs" />
+                <Gallery />
+            </main>
             <Footer />
         </div>
-    );  
-};  
+    );
+};
 
 export default Home;
