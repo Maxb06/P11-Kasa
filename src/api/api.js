1e38fa864de431754c1e7a395ffd2064d0393832
Maxime Brunet <maximb84@gmail.com>
2024-09-17 19:47:25 +0200

    style : api fetchData

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,13 @@
+/**
+ * Fetches data from /data.json and returns the parsed JSON response.
+ *
+ * If the request fails or the response is not ok, an error is thrown.
+ *
+ * If the error is caught, it is logged to the console and an empty
+ * array is returned.
+ *
+ * @returns {Promise<any[]>} The parsed JSON response, or an empty array if there is an error.
+ */
 export const fetchData = async () => {
     try {
       const response = await fetch('/data.json');
@@ -10,16 +20,3 @@ export const fetchData = async () => {
       return [];
     }
   };
-
-  export const fetchById = async (id) => {
-    try {
-      const response = await fetch(`/data.json?id=${id}`);
-      if (!response.ok) {
-        throw new Error('Erreur lors du chargement des données');
-      }
-      return await response.json();
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  }
