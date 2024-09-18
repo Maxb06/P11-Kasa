0d29660c8982c9caf98f46cf12a6168760383d44
Maxime Brunet <maximb84@gmail.com>
2024-09-18 16:52:06 +0200

    style : ajout jsdoc

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -3,16 +3,21 @@ import styles from './Collapse.module.scss';
 import arrowUp from '../../assets/arrow_up.png';
 import arrowDown from '../../assets/arrow_down.png';
 
+/**
+ * Component Collapse, qui permet de gérer l'ouverture et la fermeture 
+ * d'un élément de liste, en fonction de l'état d'un state.
+ * 
+ * @param {{title: string, content: string|Array<string>}} object - Un objet 
+ * qui contient le titre et le contenu de l'élément de liste.
+ * @returns {JSX.Element} Un élément JSX qui représente le Collapse.
+ */
 const Collapse = ({ object }) => {
-  // Utilisation du state pour gérer l'état d'ouverture du collapse
   const [isOpen, setIsOpen] = useState(false);
 
-  // Fonction pour basculer l'état du collapse
   const toggleCollapse = () => {
     setIsOpen(!isOpen);
   };
 
-  // Gestion du style de contenu en fonction de l'état
   const styleContent = {
     maxHeight: isOpen ? '500px' : '0', 
     opacity: isOpen ? '1' : '0',
@@ -25,7 +30,7 @@ const Collapse = ({ object }) => {
       <h3 className={styles.container__title} onClick={toggleCollapse}>
         {object.title}
         <img
-          src={isOpen ? arrowUp : arrowDown} // Chevrons dynamiques
+          src={isOpen ? arrowUp : arrowDown}
           alt="Toggle collapse"
           className={styles.chevron}
         />
