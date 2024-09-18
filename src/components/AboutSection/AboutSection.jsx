4b0669e62dc507f48afdea6623c11ddbe7990963
Maxime Brunet <maximb84@gmail.com>
2024-09-18 16:49:18 +0200

    style : ajout jsdoc

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,7 +1,11 @@
 import Collapse from '../Collapse/Collapse';
 import styles from './styles.module.scss';
 
-
+/**
+ * Component to display the about section, with a list of collapse elements.
+ * @param {{settling: {title: string, content: string}[]}} props - Props for the component.
+ * @returns {React.ReactElement} - The React element for the component.
+ */
 const AboutSection = ({ settling }) => {
   return (
     <main className={styles.container}>
