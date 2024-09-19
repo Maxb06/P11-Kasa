06799db23167e43b3dde1219efa4981515560ce5
Maxime Brunet <maximb84@gmail.com>
2024-09-19 14:43:31 +0200

    style : ajout redirection si ID incorrect

--- src/pages/RentPage.jsx
+++ src/pages/RentPage.jsx
@@ -1,36 +1,31 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchData } from '../api/api';
 import CardPage from '../components/CardPage/CardPage';
 
-/**
- * This component renders a rent page, given an ID from the URL.
- * It fetches the data from the API, and displays a loading message if the data is not available.
- * If the data is available, it renders the corresponding CardPage component.
- *
- * @returns {JSX.Element} - A JSX element representing the Rent page.
- */
 const Rent = () => {
   const { id } = useParams(); 
   const [house, setHouse] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    /**
-     * Fetches the data from the API, and sets the state with the selected house
-     * given the id from the URL.
-     * @returns {Promise<void>}
-     */
     const fetchHouseData = async () => {
       const data = await fetchData();
       const selectedHouse = data.find((house) => house.id === id);
-      setHouse(selectedHouse);
+
+      if (!selectedHouse) {
+        // Redirection vers la page erreur si l'id est incorrect
+        navigate('/404'); 
+      } else {
+        setHouse(selectedHouse);
+      }
     };
 
     fetchHouseData();
-  }, [id]);
+  }, [id, navigate]);
 
+  // Affiche un chargement pendant la récup des données
   if (!house) {
-    // test - affichage de 'chargement...' ou d'1 message d'erreur
     return <div>Chargement...</div>;
   }
 
