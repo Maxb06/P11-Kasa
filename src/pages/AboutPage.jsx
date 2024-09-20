a042dd5cbd8b4f92d0f2f57affa2039b28ff3097
Maxime Brunet <maximb84@gmail.com>
2024-09-20 15:56:00 +0200

    style : Optimisation - déplacement code vers /pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,26 @@
 import banner from "../assets/aboutBanner.png";
 import Banner from "../components/Banner/Banner";
 import aboutList from "../data/aboutList";
-import AboutSection from "../components/AboutSection/AboutSection";
+import Collapse from '../components/Collapse/Collapse';
+import styles from './styles/AboutPage.module.scss'; 
 
 /**
  * Returns a JSX element representing the about page.
- * It renders a banner component with the given image and an AboutSection
- * component with the given settling data.
- * The AboutSection component is defined in the AboutSection module.
+ * It renders a banner component with the given image and a list of collapse elements.
+ * 
  * @returns {React.ReactElement} - A JSX element representing the about page.
  */
 const About = () => {
     return (
         <div>
             <Banner img={banner} />
-            <AboutSection settling={aboutList} />
+            <main className={styles.container}>
+                <ul className={styles.container__content}>
+                    {aboutList.map((rule, index) => (
+                        <Collapse key={`${index}-${rule.title}`} object={rule} />
+                    ))}
+                </ul>
+            </main>
         </div>
     );
 };
--- src/pages/RentPage.jsx
+++ src/pages/RentPage.jsx
@@ -1,10 +1,18 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchData } from '../api/api';
-import CardPage from '../components/CardPage/CardPage';
+import Carrousel from '../components/Carrousel/Carrousel';
+import Collapse from '../components/Collapse/Collapse';
+import Owner from '../components/Owner/Owner';
+import HouseTitle from '../components/HouseTitle/HouseTitle';
+import styles from './styles/RentPage.module.scss'; 
 
+/**
+ * La page d'un logement.
+ * @return {JSX.Element} Un JSX element qui représente la page d'un logement.
+ */
 const Rent = () => {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const [house, setHouse] = useState(null);
   const navigate = useNavigate();
 
@@ -14,7 +22,7 @@ const Rent = () => {
       const selectedHouse = data.find((house) => house.id === id);
 
       if (!selectedHouse) {
-        // Redirection vers la page erreur si l'id est incorrect
+        // étape 9 Redirection vers la page d'erreur si l'ID est incorrect
         navigate('/404'); 
       } else {
         setHouse(selectedHouse);
@@ -24,12 +32,25 @@ const Rent = () => {
     fetchHouseData();
   }, [id, navigate]);
 
-  // Affiche un chargement pendant la récup des données
   if (!house) {
-    return <div>Chargement...</div>;
+    return <div className={styles.loading}>Chargement...</div>;
   }
 
-  return <CardPage house={house} />;
+  const { description, equipments, host, location, pictures, rating, tags, title } = house;
+
+  return (
+    <main className={styles.main}> 
+      <Carrousel pictures={pictures} title={title} />
+      <section className={styles.containerInfo}> 
+        <HouseTitle title={title} location={location} tags={tags} />
+        <Owner host={host} rating={rating} />
+      </section>
+      <ul className={styles.main__dropdown}>
+        <Collapse object={{ title: 'Description', content: description }} />
+        <Collapse object={{ title: 'Équipements', content: equipments }} />
+      </ul>
+    </main>
+  );
 };
 
 export default Rent;
